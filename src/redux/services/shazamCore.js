d6600f5cfbe8683f654d0f5ce09e7b17b6408093
Prakash Tripathi <prakasht@zeta.tech>
2022-10-09 14:30:24 +0530

    rapid client API added

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'pcZZf9n0sqmshSrlSQWnMuf2OLkpp1CpdI7jsntYl8i0CYDVzq',
+      'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+export const shazamCoreApi = createApi({
+    reducerPath : 'shazamCoreApi'
+})
